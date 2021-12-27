d1d861dbd9f61811ca708b7821f22c223352cf2b
Arya Dewa <83571071+Aryadw@users.noreply.github.com>
2021-12-27 19:37:53 +0700

    Popup Sign up & Login
    Add Popup Sign up & Login

---
+++ Popup sign-up/README.markdown
@@ -0,0 +1,5 @@
+# Sign Up
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/aryadw/pen/jOGYMOw](https://codepen.io/aryadw/pen/jOGYMOw).
+
+
---
+++ Popup sign-up/dist/script.js
@@ -0,0 +1,4 @@
+document.querySelector("#show-signup").addEventListener("click",function(){document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click",function(){ document.querySelector(".popup").classList.remove("active");
+});
